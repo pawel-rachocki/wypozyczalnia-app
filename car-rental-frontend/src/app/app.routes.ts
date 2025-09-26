fd824b1e536fe18d5f167c956f07fe3eff25f5c3
Pawel <pawel.logo@gmail.com>
2025-09-26 13:30:24 +0200

    WYP-29 Komponenty zarządzania samochodami (lista + formularz)

--- car-rental-frontend/src/app/app.routes.ts
+++ car-rental-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {SamochodListComponent} from './components/samochody/samochod-list/samochod-list.component';
+import {SamochodFormComponent} from './components/samochody/samochod-form/samochod-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/samochody', pathMatch: 'full' },
+  { path: 'samochody', component: SamochodListComponent },
+  { path: 'samochody/new', component: SamochodFormComponent },
+  { path: 'samochody/edit/:id', component: SamochodFormComponent }
+];
