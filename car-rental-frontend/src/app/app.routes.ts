2ed5156c4fbaca617567e03234c1f1b1b413b676
Pawel <pawel.logo@gmail.com>
2025-09-26 14:02:32 +0200

    WYP-30 Komponenty zarządzania klientami (lista + formularz)

--- car-rental-frontend/src/app/app.routes.ts
+++ car-rental-frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
-import {SamochodListComponent} from './components/samochody/samochod-list/samochod-list.component';
-import {SamochodFormComponent} from './components/samochody/samochod-form/samochod-form.component';
+import { SamochodListComponent } from './components/samochody/samochod-list/samochod-list.component';
+import { SamochodFormComponent } from './components/samochody/samochod-form/samochod-form.component';
+import { KlientListComponent } from './components/klienci/klient-list/klient-list.component';
+import { KlientFormComponent } from './components/klienci/klient-form/klient-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/samochody', pathMatch: 'full' },
   { path: 'samochody', component: SamochodListComponent },
   { path: 'samochody/new', component: SamochodFormComponent },
-  { path: 'samochody/edit/:id', component: SamochodFormComponent }
+  { path: 'samochody/edit/:id', component: SamochodFormComponent },
+  { path: 'klienci', component: KlientListComponent },
+  { path: 'klienci/new', component: KlientFormComponent },
+  { path: 'klienci/edit/:id', component: KlientFormComponent }
 ];
