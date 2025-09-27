807bb5f9d43b5d27e39cbb2158b02a932ad70170
Pawel <pawel.logo@gmail.com>
2025-09-27 02:28:33 +0200

    WYP-40 Cleanup Repo/service/controller klient

--- car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/controller/SamochodController.java
+++ car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/controller/SamochodController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
--- car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/repository/SamochodRepository.java
+++ car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/repository/SamochodRepository.java
@@ -3,12 +3,9 @@ package com.wypozyczalnia.car_rental_backend.repository;
 import com.wypozyczalnia.car_rental_backend.entity.Samochod;
 import com.wypozyczalnia.car_rental_backend.entity.StatusSamochodu;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+
 
 public interface SamochodRepository extends JpaRepository<Samochod,Long> {
     List<Samochod> findByStatusOrderByMarkaAscModelAsc(StatusSamochodu status);
