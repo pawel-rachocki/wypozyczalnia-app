122c08047d1887dee0132d0938b14361ac1fdb40
Pawel <pawel.logo@gmail.com>
2025-09-27 14:02:04 +0200

    WYP-48 Testy SamochodRepository

--- car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/repository/SamochodRepository.java
+++ car-rental-backend/src/main/java/com/wypozyczalnia/car_rental_backend/repository/SamochodRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface SamochodRepository extends JpaRepository<Samochod,Long> {
     List<Samochod> findByStatusOrderByMarkaAscModelAsc(StatusSamochodu status);
+    List<Samochod> findByMarkaIgnoreCaseOrderByModelAsc(String marka);
 
     boolean existsByMarkaIgnoreCaseAndModelIgnoreCase(String marka, String model);
 
